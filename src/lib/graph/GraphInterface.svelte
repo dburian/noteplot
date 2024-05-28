8fecae89d039a6dbc9a6c7fa71f30bfa0a0b4446
David Burian <david.burian@me.com>
2024-05-28 15:31:17 +0200

    Simple working Toolbar

--- src/lib/graph/GraphInterface.svelte
+++ src/lib/graph/GraphInterface.svelte
@@ -1,75 +1,26 @@
 <script>
   import { goto } from '$app/navigation';
   import ToolbarButton from '$lib/toolbar/ToolbarButton.svelte';
-  import { onMount } from 'svelte';
   import Toolbar from '../toolbar/Toolbar.svelte';
   import Graph from './Graph.svelte';
-  import { createGraphState } from './graphInterfaceState';
 
   export let graphProps;
   export let withGraph = true;
   export let viewedNote = null;
   export let graphFullScreen = false;
 
-  const dynamicState = createGraphState({ withGraph, viewedNote, graphFullScreen });
-  $: dynamicState.update({withGraph, viewedNote, graphFullScreen})
-
 
   let container = null;
-  let slider = null;
-  let toolbar;
-
-  const mouseMove = (event) => {
-    slider.style.cssText = `position: absolute;top: ${event.clientY - slider.clientHeight / 2}px; left: ${event.clientX - slider.clientWidth / 2}px;`;
-    const maxCursorWidth = container.clientWidth - toolbar.clientWidth;
-    const snapMargin = 50;
-    let cursorWidth = Math.max(
-      0,
-      Math.min(maxCursorWidth, event.clientX - toolbar.clientWidth / 2)
-    );
-
-    if (cursorWidth < snapMargin) {
-      cursorWidth = 0;
-    }
-
-    if (maxCursorWidth - cursorWidth < snapMargin) {
-      cursorWidth = maxCursorWidth;
-    }
-
-    dynamicState.update({ setWidth: cursorWidth });
-  };
-
-  const cancelMove = () => {
-    dynamicState.update((state) => ({
-      sliding: false,
-      withGraph: state.setWidth > 0,
-      graphFullScreen: state.setWidth == container.clientWidth - toolbar.clientWidth
-    }));
-    container.removeEventListener('mousemove', mouseMove);
-    container.removeEventListener('mouseup', cancelMove);
-    container.removeEventListener('mouseleave', cancelMove);
-    if (slider !== null) {
-      slider.style = '';
-    }
-  };
-
-  const onSliderMousedown = (event) => {
-    dynamicState.update({ sliding: true });
-    mouseMove(event);
-    container.addEventListener('mousemove', mouseMove);
-    container.addEventListener('mouseup', cancelMove);
-    container.addEventListener('mouseleave', cancelMove);
-  };
 </script>
 
 <div class="flex h-screen graph-note-container overflow-hidden relative" bind:this={container}>
   <div
-    class="graph-container h-screen"
-    style={$dynamicState.width !== null ? `width: ${$dynamicState.width}px;flex: none;` : ''}
+    class="h-screen"
+    style={withGraph ? "" : "width: 0px;flex:none"}
   >
     <Graph {...graphProps} {viewedNote} />
   </div>
-  <Toolbar bind:toolbar>
+  <Toolbar>
     {#if viewedNote}
       <ToolbarButton on:click={() => goto('/')}>x</ToolbarButton>
     {/if}
@@ -88,22 +39,11 @@
         {'>'}
       </ToolbarButton>
     {/if}
-    {#if viewedNote }
-      <ToolbarButton bind:button={slider} on:mousedown={onSliderMousedown}>
-        {'<->'}
-      </ToolbarButton>
-    {/if}
   </Toolbar>
   <div
-    class="flex-1 overflow-auto"
+    class="w-[45%] min-w-[40ch] overflow-auto"
     style={!viewedNote || graphFullScreen ? 'flex: none; width:0px' : ''}
   >
     <slot />
   </div>
 </div>
-
-<style>
-  .graph-container {
-    flex: 2 1 0%;
-  }
-</style>
--- src/lib/toolbar/Toolbar.svelte
+++ src/lib/toolbar/Toolbar.svelte
@@ -1,10 +1,5 @@
-<script>
-  export let toolbar;
-</script>
-
 <div
   class='h-screen grid auto-rows-max gap-y-4 p-4'
-  bind:this={toolbar}
 >
   <slot />
 </div>
