9d8397e4cc0ec22e1b460c849c787ed3419823b9
David Burian <david.burian@me.com>
2024-10-18 20:21:24 +0200

    search: search toolbar button gets an icon

--- src/lib/toolbar/ToolbarButton.svelte
+++ src/lib/toolbar/ToolbarButton.svelte
@@ -1,15 +1,16 @@
 <script>
-    import { getContext } from "svelte";
+  import { getContext } from 'svelte';
 
   export let button = null;
 
-  const adaptive = getContext("adaptive")
+  const adaptive = getContext('adaptive');
 </script>
 
 <button
   on:click
   on:mousedown
   bind:this={button}
-  class={`${$adaptive.mobile ? 'w-12 h-12': 'w-10 h-10'} grid content-center bg-white dark:bg-neutral-800 border-2 font-bold border-black dark:border-invert-white select-none dark:text-white hover:bg-black dark:hover:bg-invert-white hover:text-white dark:hover:text-neutral-800 text-base sm:text-base`}>
+  class={`${$adaptive.mobile ? 'w-12 h-12' : 'w-10 h-10'} grid content-center bg-white dark:bg-neutral-800 border-2 font-bold border-black dark:border-invert-white select-none dark:text-white hover:bg-black dark:hover:bg-invert-white hover:text-white dark:hover:text-neutral-800 text-base sm:text-base stroke-black dark:stroke-invert-white hover:stroke-white hover:dark:stroke-neutral-800 p-2`}
+>
   <slot />
 </button>
--- src/lib/toolbar/buttons/Search.svelte
+++ src/lib/toolbar/buttons/Search.svelte
@@ -1,11 +1,10 @@
 <script>
-    import { createEventDispatcher, getContext } from "svelte";
-    import ToolbarButton from "../ToolbarButton.svelte";
+  import { createEventDispatcher, getContext } from 'svelte';
+  import ToolbarButton from '../ToolbarButton.svelte';
 
-  const giState = getContext("graphInterfaceState")
-  const dispatcher = createEventDispatcher()
+  const giState = getContext('graphInterfaceState');
+  const dispatcher = createEventDispatcher();
 </script>
-
 
 {#if $giState.search === null}
   <ToolbarButton
@@ -14,10 +13,26 @@
         search: '',
         viewedNote: null,
         graphFullScreen: false
-      })
-      dispatcher('click')
+      });
+      dispatcher('click');
     }}
   >
-    ?
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 136 137"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_5_3)">
+        <rect x="61" y="10" width="65" height="65" stroke-width="20" />
+        <line x1="58.0711" y1="78.0711" x2="7.07107" y2="129.071" stroke-width="20" />
+      </g>
+      <defs>
+        <clipPath id="clip0_5_3">
+          <rect width="136" height="137" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
   </ToolbarButton>
 {/if}
