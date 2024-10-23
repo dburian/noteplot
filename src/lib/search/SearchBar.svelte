05af91c798dbd06591c7ce4d48ef7ab412811607
David Burian <david.burian@me.com>
2024-10-23 18:30:09 +0200

    don't ever x-scroll search window

--- src/lib/search/SearchBar.svelte
+++ src/lib/search/SearchBar.svelte
@@ -28,7 +28,7 @@
   <div class="border-2 w-full border-black dark:border-invert-white flex text-xl mb-6">
     <input
       type="text"
-      class="p-1 flex-1 focus-visible:outline-none dark:bg-neutral-800"
+      class="p-1 w-full flex-1 focus-visible:outline-none dark:bg-neutral-800"
       placeholder="Search for something..."
       bind:value={searchString}
       on:keydown={debounceSearch}
