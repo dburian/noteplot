e964506deb431f88f2d5ef8c0dd6b2e29b487f34
David Burian <david.burian@me.com>
2024-11-28 16:06:40 +0100

    on/off search and filter buttons

--- src/lib/toolbar/buttons/Filter.svelte
+++ src/lib/toolbar/buttons/Filter.svelte
@@ -3,32 +3,31 @@
   import { page } from '$app/stores';
     import DesktopToolbarButton from '../DesktopToolbarButton.svelte';
 
-  $: searchString = $page.url.searchParams.get('q') || null;
+  $: filterActive = $page.url.pathname == '/filter'
 </script>
 
-{#if searchString === null}
-  <DesktopToolbarButton
-    activeColor="bg-emerald-600 dark:bg-emerald-500"
-    defaultColor="bg-emerald-800 dark:bg-emerald-700"
-    label="Filter notes"
-    on:click={() => goto('/filter?q=')}
+<DesktopToolbarButton
+  activeColor="bg-emerald-600 dark:bg-emerald-500"
+  defaultColor="bg-emerald-800 dark:bg-emerald-700"
+  on={filterActive}
+  label="Filter notes"
+  on:click={() => goto('/filter?q=')}
+>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 136 137"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
   >
-    <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 136 137"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clip-path="url(#clip0_5_3)">
-        <rect x="61" y="10" width="65" height="65" stroke-width="20" />
-        <line x1="58.0711" y1="78.0711" x2="7.07107" y2="129.071" stroke-width="20" />
-      </g>
-      <defs>
-        <clipPath id="clip0_5_3">
-          <rect width="136" height="137" fill="white" />
-        </clipPath>
-      </defs>
-    </svg>
-  </DesktopToolbarButton>
-{/if}
+    <g clip-path="url(#clip0_5_3)">
+      <rect x="61" y="10" width="65" height="65" stroke-width="20" />
+      <line x1="58.0711" y1="78.0711" x2="7.07107" y2="129.071" stroke-width="20" />
+    </g>
+    <defs>
+      <clipPath id="clip0_5_3">
+        <rect width="136" height="137" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+</DesktopToolbarButton>
--- src/lib/toolbar/buttons/Search.svelte
+++ src/lib/toolbar/buttons/Search.svelte
@@ -1,34 +1,33 @@
 <script>
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
-    import DesktopToolbarButton from '../DesktopToolbarButton.svelte';
+  import DesktopToolbarButton from '../DesktopToolbarButton.svelte';
 
-  $: searchString = $page.url.searchParams.get('q') || null;
+  $: searchActive = $page.url.pathname === '/search';
 </script>
 
-{#if searchString === null}
-  <DesktopToolbarButton
-    activeColor="bg-green-600 dark:bg-green-500"
-    defaultColor="bg-green-800 dark:bg-green-700"
-    label="Search notes"
-    on:click={() => goto('/search?q=')}
+<DesktopToolbarButton
+  activeColor="bg-green-600 dark:bg-green-500"
+  defaultColor="bg-green-800 dark:bg-green-700"
+  on={searchActive}
+  label="Search notes"
+  on:click={() => goto('/search?q=')}
+>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 136 137"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
   >
-    <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 136 137"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clip-path="url(#clip0_5_3)">
-        <rect x="61" y="10" width="65" height="65" stroke-width="20" />
-        <line x1="58.0711" y1="78.0711" x2="7.07107" y2="129.071" stroke-width="20" />
-      </g>
-      <defs>
-        <clipPath id="clip0_5_3">
-          <rect width="136" height="137" fill="white" />
-        </clipPath>
-      </defs>
-    </svg>
-  </DesktopToolbarButton>
-{/if}
+    <g clip-path="url(#clip0_5_3)">
+      <rect x="61" y="10" width="65" height="65" stroke-width="20" />
+      <line x1="58.0711" y1="78.0711" x2="7.07107" y2="129.071" stroke-width="20" />
+    </g>
+    <defs>
+      <clipPath id="clip0_5_3">
+        <rect width="136" height="137" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+</DesktopToolbarButton>
